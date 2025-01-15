d8bca8dabf76fc43f7920f0fc3bb26e9367b8b0e
Joldosh <joldosh5577@gmail.com>
2025-01-16 00:46:39 +0600

    Addem some changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        // https://github.com/Joldosh23/AQA-1
+        //git config --global user.email
+        //git init
+
+        //git config --global user.email "you@example.com"
+        //  git config --global user.name "Your Name"
+        //  git config --global user.email "joldosh5577@gmail.com"
+        // git config --global user.name"Joldosh"
+
+        // 1) git config --global user.name "Izat"
+// 2) git config --global user.email "sss@gmail.com"
+// 3) git init
+// 4) git remote add origin https://github.com/IzatKG/AQA-Automation
+//5)  git add .
+//6)  git commit -m "Addem some changes"
+// 7)  git push origin master
+
+    }
+}
